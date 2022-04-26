e802ed006b0653c98de6b6f23e93c3d74565bb66
Kris Baily <81344693+Kristerran@users.noreply.github.com>
2022-04-25 22:02:59 -0700

    Finishing merge and fixing minor routing issue

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,6 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN } from '../utils/mutations';
-<<<<<<< HEAD
-
-import Auth from '../utils/auth';
-function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error }] = useMutation(LOGIN);
-=======
 import { useNavigate, useLocation, Navigate } from 'react-router-dom';
 
 export default function LoginPage() {
@@ -21,7 +14,6 @@ export default function LoginPage() {
   const [login, { error, data, loading }] = useMutation(LOGIN);
   const onChange = (e) =>
     setCredentials({ ...loginCredentials, [e.target.name]: e.target.value });
->>>>>>> a3e4a346697962ccee0dc6ef877f0b02a1f07b28
 
   const loginProcess = async () => {
     if (loginCredentials.email && loginCredentials.password) {
@@ -38,34 +30,6 @@ export default function LoginPage() {
     }
   };
   return (
-<<<<<<< HEAD
-    <div className="container my-1">
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-=======
     <div className="base">
       <div className="loginHeader">Login</div>
       <div className="content">
@@ -93,7 +57,6 @@ export default function LoginPage() {
               value={loginCredentials.password}
               placeholder="Password"
             />
->>>>>>> a3e4a346697962ccee0dc6ef877f0b02a1f07b28
           </div>
         </div>
       </div>
