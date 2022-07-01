d12049197697820866ccc0bda952efd479564d95
Kris Baily <81344693+Kristerran@users.noreply.github.com>
2022-07-01 13:16:29 -0700

    Making transition to MUI

---
+++ client/src/components/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+// import Container from '@mui/material/Container';
+import { AppBar,Typography, IconButton } from "@mui/material";
+import Toolbar from '@mui/material/Toolbar';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link as RouterLink,
+} from "react-router-dom";
+const Header = () => {
+  return (
+    <AppBar position="static">
+          <Toolbar>
+            <IconButton component={RouterLink} to="/">
+              <Typography variant="h6">Home</Typography>
+            </IconButton>
+            <IconButton component={RouterLink} to="/login">
+              <Typography variant="h6">Login</Typography>
+            </IconButton>
+            <IconButton component={RouterLink} to="/signup">
+              <Typography variant="h6">Signup</Typography>
+            </IconButton>
+            <IconButton component={RouterLink} to="/characters">
+              <Typography variant="h6">Characters</Typography>
+            </IconButton>
+            <IconButton component={RouterLink} to="/journal">
+              <Typography variant="h6">Journal</Typography>
+            </IconButton>
+            <IconButton component={RouterLink} to="/roster">
+              <Typography variant="h6">Roster</Typography>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+  );
+};
+
+export default Header;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import '../App.css';
+import React from "react";
+// import Container from '@mui/material/Container';
+import { Container, Typography, Card, CardContent } from "@mui/material";
+import "../App.css";
 
 const Home = () => {
   return (
-    <div className="container">
-      <Button variant="text">ADVENTURNAL</Button>
-      <div className="LargeHeader">
-        Welcome to Adventurnal, your tabletop campaign journal, upgraded.
-      </div>
-      <div classname="informationContainer">
-        {/* <img className="questPic" src="/images/quest.jpg"></img> */}
-        <div classname="informaionText">
-          We aim to provide a location for all your record keeping, helping you
-          manage your characters. Keep a personal account of your sessions for
-          you to refer in the downtime between sessions. Log details about your
-          character and tag them for easier call backs.
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Typography variant="h1">WELCOME TO ADVENTURNAL</Typography>
+      <Typography variant="h2">
+        your tabletop campaign journal, upgraded.
+      </Typography>
+      <Typography variant="h5">
+        We aim to provide a location for all your record keeping, helping you
+        manage your characters. Keep a personal account of your sessions for you
+        to refer in the downtime between sessions. Log details about your
+        character and tag them for easier call backs.
+      </Typography>
+    </Container>
   );
 };
 
